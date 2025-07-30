c7e91b78015103997bf6d6565f5004a1d1b65749
Omar-Hesham-Mohamed <omarhesham2776@gmail.com>
2025-07-30 19:23:19 +0300

    All Tasks Completed and Tested

--- Race.py
+++ Race.py
@@ -5,7 +5,7 @@ class Race:
     def __init__(self):
         self.max = Verstappen()
         self.mostafa = Mostafa()
-        self.roundNo = 1
+        self.roundNo = 0
         self.maxTurn = True
         self.lastDamage = 0
     
@@ -15,34 +15,45 @@ class Race:
 
             if self.maxTurn:
                 print("It's Max's turn.")
-                self.maxTurn = False
                 tactic = input("Are you going to attack or defend, Max? Enter 1 for attack, 2 for defend: ")
                 if tactic == '1':
                     self.max._tireHealth = self.lastDamage
-                    move = input("What is your move, Max? Enter a number: ")
+                    move = int(input("What is your move, Max? Enter a number: "))
+                    while move < 1 or move > 3:
+                        move = int(input("Enter a number (1-3): ")) 
                     self.lastDamage = self.max.offence(int(move))
+                    self.maxTurn = False
                 elif tactic == '2':
-                    move = input("What is your move, Max? Enter a number: ")
+                    move = int(input("What is your move, Max? Enter a number: "))
                     defence = self.max.defence(int(move))
+                    while move < 1 or move > 2:
+                        move = int(input("Enter a number (1-2): "))
                     self.max._tireHealth = (1 - defence) * self.lastDamage
+                    self.maxTurn = False
                 else:
                     print("Invalid input. Please enter 1 or 2.")
-                    self.maxTurn = True
+                    continue
             else:
                 print("It's Mostafa's turn.")
-                self.maxTurn = True
                 tactic = input("Are you going to attack or defend, Mostafa? Enter 1 for attack, 2 for defend: ")
                 if tactic == '1':
                     self.mostafa._tireHealth = self.lastDamage
+                    move = int(input("What is your move, Mostafa? Enter a number: "))
+                    while move < 1 or move > 3:
+                        move = int(input("Enter a number (1-3): "))
                     move = input("What is your move, Mostafa? Enter a number: ")
                     self.lastDamage = self.mostafa.offence(int(move))
+                    self.maxTurn = True
                 elif tactic == '2':
-                    move = input("What is your move, Mostafa? Enter a number: ")
+                    move = int(input("What is your move, Mostafa? Enter a number: "))
+                    while move < 1 or move > 2:
+                        move = int(input("Enter a number (1-2): "))
                     defence = self.mostafa.defence(int(move))
                     self.mostafa._tireHealth = (1 - defence) * self.lastDamage
+                    self.maxTurn = True
                 else:
                     print("Invalid input. Please enter 1 or 2.")
-                    self.maxTurn = False
+                    continue
 
             print(f"Round {self.roundNo + 1} complete.")
             self.roundNo += 1
@@ -54,4 +65,6 @@ class Race:
         elif self.mostafa._tireHealth <= 0:
             print("Max wins!")
 
-    
+
+myRace = Race()
+myRace.start()    
